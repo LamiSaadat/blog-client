e43a7fb8095c3c89d7805603a8a60b340b14e791
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-11 16:37:33 -0500

    Created nav bar with routes for login page and feed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-// import Feed from "../Feed/Feed";
 
 function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/">Feed</Link>
-        </li>
-        <li>
-          <Link to="/login">Log In</Link>
-        </li>
-      </ul>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component={Link} sx={{ flexGrow: 1 }} to="/">
+            Home
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Feed
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
