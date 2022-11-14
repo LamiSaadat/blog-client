440ba203afde3c14219d906e877f8a31626b84c8
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-14 14:28:45 -0500

    Removed material ui components and uninstalled material ui. Installed bootstrap to reduce styling time to build this project

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,7 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
 
 function Nav() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component={Link} sx={{ flexGrow: 1 }} to="/">
-            Home
-          </Typography>
-          <Button color="inherit" component={Link} to="/">
-            Feed
-          </Button>
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  return <div>Nav</div>;
 }
 
 export default Nav;
