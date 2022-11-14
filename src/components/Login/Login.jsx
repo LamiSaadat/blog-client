a3b60efd6fdebb3806fca155ff4529f3aea3b9a6
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-14 14:54:24 -0500

    Created login page with bootstrap form and required styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Login.scss";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Container className="login-container">
+      <Form>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 }
 
 export default Login;
