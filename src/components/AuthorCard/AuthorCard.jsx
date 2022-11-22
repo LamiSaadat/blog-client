638abf2a7ecc80d6de6d7e61f6e35b61f81bdebb
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-22 12:52:24 -0500

    Added styles to author card

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -1,16 +1,14 @@
 import React from "react";
+import "./AuthorCard.scss";
 import Card from "react-bootstrap/Card";
 import FollowButton from "../FollowButton/FollowButton";
 
 function AuthorCard() {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card content.
-        </Card.Text>
+    <Card className="author-card">
+      <Card.Body className="author-card__body">
+        <Card.Title className="author-card__username">User Name</Card.Title>
+        <Card.Text className="author-card__followers">followers</Card.Text>
         <FollowButton />
       </Card.Body>
     </Card>
