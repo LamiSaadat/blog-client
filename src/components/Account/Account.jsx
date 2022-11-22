c3f28b4c48edef840758224063518781bbcf36c8
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-11-22 15:07:16 -0500

    Updated user card to show logged in user's details

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,26 +1,14 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getUserProfile } from "../../features/user/userSlice";
+import React from "react";
 import UserCard from "../UserCard/UserCard";
 import PostCard from "../PostCard/PostCard";
 
-function Protected() {
-  const { userInfo, accesstoken } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (accesstoken) {
-      dispatch(getUserProfile());
-    }
-  }, [accesstoken, dispatch]);
-
+function Account() {
   return (
     <div>
-      <div>{userInfo.email}</div>
       <UserCard />
       <PostCard />
     </div>
   );
 }
 
-export default Protected;
+export default Account;
--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "react-bootstrap/Card";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProfile } from "../../features/user/userSlice";
 import FollowButton from "../FollowButton/FollowButton";
 
 function UserCard() {
+  const { userInfo, accesstoken } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (accesstoken) {
+      dispatch(getUserProfile());
+    }
+  }, [accesstoken, dispatch]);
+
   return (
     <Card>
       <Card.Body>
-        <Card.Title>User name</Card.Title>
-        <Card.Text>followers</Card.Text>
-        <Card.Text>posts</Card.Text>
+        <Card.Title>{userInfo?.firstName}</Card.Title>
+        <Card.Text>Followers {userInfo?.followedBy?.length}</Card.Text>
+        <Card.Text>Following {userInfo?.following?.length}</Card.Text>
+        <Card.Text>Posts {userInfo?.posts?.length}</Card.Text>
         <FollowButton />
       </Card.Body>
     </Card>
