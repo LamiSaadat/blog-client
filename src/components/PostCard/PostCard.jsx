c225052918a40506219835bf1e8bd8cf7f07f477
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-12-01 16:19:19 -0500

    Updated blog post card with bootstrap styles

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import { NavLink } from "react-router-dom";
-import LikeButton from "../LikeButton/LikeButton";
+// import LikeButton from "../LikeButton/LikeButton";
 
 function PostCard({ allPosts }) {
   const posts = allPosts.map((post) => (
-    <div key={post?.id}>
-      <p>{post?.title}</p>
-      <p>{post?.content.slice(0, 20)}</p>
-      <NavLink
-        to={`/blogPost/${post?.id}`}
-        state={{ from: "all posts", item: post }}
-      >
-        <button type="submit">View Post</button>
-      </NavLink>
-      <LikeButton />
+    <div key={post?.id} className="col">
+      <div className="card shadow-sm">
+        <h2 style={{ padding: "1rem" }}>{post?.title}</h2>
+        <div className="card-body">
+          <p className="card-text">{post?.content.slice(0, 20)}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <NavLink
+                to={`/blogPost/${post?.id}`}
+                state={{ from: "all posts", item: post }}
+              >
+                <button
+                  type="button"
+                  className="btn btn-sm btn-outline-secondary"
+                >
+                  View Post
+                </button>
+              </NavLink>
+              {/* <button type="button" className="btn btn-sm btn-outline-secondary">
+              Edit
+            </button> */}
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
     </div>
   ));
 
   return (
-    <div style={{ width: "18rem" }} key={posts?.id}>
+    <div
+      key={posts?.id}
+      className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+      style={{ marginTop: "1rem" }}
+    >
       {posts}
     </div>
   );
